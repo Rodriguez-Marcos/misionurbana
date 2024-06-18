837c4522f6cf476d1e5d11c6ebdf7acc6cc3c81c
Marcos <84048657+Rodriguez-Marcos@users.noreply.github.com>
2024-06-17 22:48:42 -0300

    formateado y arreglado

--- src/navigation.js
+++ src/navigation.js
@@ -33,9 +33,14 @@ export const footerData = {
     {
       title: 'Contactos',
       links: [
-        { text: 'WhatsApp', href: 'https://wa.me/5493512291058', ariaLabel: 'Contacto WhatsApp', icon: 'tabler:whatsapp' },
-      ]
-    }
+        {
+          text: 'WhatsApp',
+          href: 'https://wa.me/5493512291058',
+          ariaLabel: 'Contacto WhatsApp',
+          icon: 'tabler:whatsapp',
+        },
+      ],
+    },
   ],
   footNote: `Hecho con ❤️ para Jesús - Equipo misión urbana - Departamento de Misiones - Iglesia Cita con la Vida`,
 };
